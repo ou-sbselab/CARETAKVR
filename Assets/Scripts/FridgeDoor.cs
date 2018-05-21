acab0b3dc520eadd3feae544606997f49e45e482
Nick K <Gocnak@users.noreply.github.com>
2018-05-21 11:18:38 -0400

    initial push of game

---
+++ Assets/Scripts/FridgeDoor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FridgeDoor : MonoBehaviour {
+
+
+    public Animation anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.E))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.name == "Door")
+                {
+                     anim.Play("DoorOpen");        
+                }
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/StoveButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DigitalRuby.PyroParticles;
+
+public class StoveButton : MonoBehaviour {
+
+    public GameObject fire;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hellooooo!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.E))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.name);
+                if (hit.collider.name == "Stove")
+                {
+                     fire.SetActive(!fire.activeSelf);          
+                }
+            }
+        }  
+	}
+}
